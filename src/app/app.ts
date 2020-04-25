a0596efe238b51fa1e938b4d92cf62a69d2a15fb
depidsvy <spidgorny@gmail.com>
2020-04-26 00:32:41 +0200

    - add proper light

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,15 @@
-import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+import {
+  AmbientLight,
+  Color, Mesh, MeshBasicMaterial,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SphereBufferGeometry,
+  SpotLight,
+  SpotLightShadow,
+  Vector3,
+  WebGLRenderer
+} from 'three';
 import { Brick } from './brick';
 
 export class App {
@@ -10,13 +21,30 @@ export class App {
   });
 
   private brick: Brick;
+  private ambient: AmbientLight;
+  private light: PointLight;
+  private runner: number = 0;
 
   constructor() {
-    this.brick = new Brick(100, new Color('rgb(255,0,0)'));
+    this.brick = new Brick(40, new Color('rgb(255,0,0)'));
     this.scene.add(this.brick);
 
-    this.camera.position.set(200, 200, 200);
+    const brick2 = new Brick(40, new Color('rgb(0,0,255)'));
+    brick2.position.set(50, 0, 0);
+    this.scene.add(brick2);
+
+    this.camera.position.set(100, 200, 200);
     this.camera.lookAt(new Vector3(0, 0, 0));
+
+    this.ambient = new AmbientLight(0xffffff, 0.5);
+    this.scene.add(this.ambient);
+
+    this.light = new PointLight(0xffffff, 0.5, 10000, 0);
+    this.light.position.set( 80, 80, 50 );
+    const sphere = new SphereBufferGeometry( 1, 8, 8 );
+    this.light.add( new Mesh( sphere, new MeshBasicMaterial( { color: 0xff0040 } ) ) );
+
+    this.scene.add(this.light);
 
     this.renderer.setSize(innerWidth, innerHeight);
     this.renderer.setClearColor(new Color('rgb(0,0,0)'));
@@ -35,6 +63,8 @@ export class App {
     requestAnimationFrame(() => this.render());
 
     this.adjustCanvasSize();
-    this.brick.rotateY(0.03);
+    this.brick.rotateY(0.01);
+    this.camera.position.set(100, 200 + 10 * Math.sin(this.runner/100), 200);
+    this.runner += 1;
   }
 }
